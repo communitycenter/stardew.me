5e11a9fc37a472782f43cfbbf063818e8a69faa5
Clemente Solorio <solorioeclem164@gmail.com>
2023-07-20 00:17:23 -0700

    local testing file

---
+++ local.py
@@ -0,0 +1,18 @@
+import sys
+
+from lib.tools import createPlayer
+from lib.assets import loadAvatarAssets
+from lib.renderer import CharacterRenderer
+
+
+if __name__ == "__main__":
+    file_path = "./saves/yuh"
+
+    if len(sys.argv) > 1:
+        file_path = "./saves/" + sys.argv[1]
+
+    assets = loadAvatarAssets()
+    player = createPlayer(file_path)
+    Avatar = CharacterRenderer(player, assets)
+    avatar = Avatar.render()
+    avatar.save("test.png")
