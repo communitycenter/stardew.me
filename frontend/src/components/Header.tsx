40ed5da29dde2800213d8f2b8920691c10ffb7ec
Jack LaFond <jack.lafond@icloud.com>
2023-07-27 21:25:29 -0400

    feat: have social links in mobile sidebar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -95,6 +95,21 @@ export default function Header() {
                 </Disclosure.Button>
               ))}
             </div>
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {extLinks.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
           </Disclosure.Panel>
         </>
       )}
