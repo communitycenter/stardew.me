6d220761bf485e964edccd405cb32cad8969a037
Jack LaFond <jack.lafond@icloud.com>
2023-07-27 23:17:07 -0400

    may stackoverflow grant me peace

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import hashlib
 import json
 
 from io import BytesIO
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
@@ -36,6 +36,30 @@ s3 = boto3.client(
 )
 
 app = FastAPI()
+
+# Salt to your taste
+ALLOWED_ORIGINS = "*"  # or 'foo.com', etc.
+
+
+# handle CORS preflight requests
+@app.options("/{rest_of_path:path}")
+async def preflight_handler(request, rest_of_path):
+    response = Response()
+    response.headers["Access-Control-Allow-Origin"] = ALLOWED_ORIGINS
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+    return response
+
+
+# set CORS headers
+@app.middleware("http")
+async def add_CORS_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = ALLOWED_ORIGINS
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+    return response
+
 
 assets = loadAvatarAssets()
 sleeveless_shirts = get_sleeveless_shirts()
