0f1c94d44a1cc48a8a5c96e0f1203330e1356987
Jack LaFond <jack.lafond@icloud.com>
2023-07-27 22:40:09 -0400

    ??? (jackson said to do this) ???

--- backend/main.py
+++ backend/main.py
@@ -39,10 +39,23 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://stardew.me"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    expose_headers=["set-cookie"],  # Expose the "Set-Cookie" header to the browser
+    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
 )
 
 assets = loadAvatarAssets()
