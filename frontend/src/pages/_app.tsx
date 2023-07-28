961bca3000b6f249ee384a498f7e29d981406b54
Jack LaFond <jack.lafond@icloud.com>
2023-07-28 00:03:04 -0400

    feat: add fathom analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,22 +1,52 @@
 import * as React from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "next-themes";
 import { AvatarProvider } from "../lib/AvatarContext";
-import { UploadProvider } from '../lib/UploadContext';
+import { UploadProvider } from "../lib/UploadContext";
+import * as Fathom from "fathom-client";
 
 import "../styles/globals.css";
 
 import { Toaster } from "@/components/ui/toaster";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("AKVRASRP", {
+      includedDomains: ["stardew.me"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <UploadProvider>
         <AvatarProvider>
           <ThemeProvider attribute="class">
             <Head>
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+              />
             </Head>
             <main>
               <Component {...pageProps} />
