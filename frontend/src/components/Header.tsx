6218c369f7f05f102ab3322f0d6db31b25627713
Jack LaFond <jack.lafond@icloud.com>
2023-07-27 23:46:49 -0400

    just chuck it behind cdn

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -42,7 +42,7 @@ export default function Header() {
                     width={"36"}
                     height={"36"}
                     className="h-9 w-auto"
-                    src="../assets/logo.png"
+                    src="https://stardew.me/assets/logo.png"
                     alt="stardew.me logo"
                   />
                   <h1 className="pl-4 font-medium">stardew.me</h1>
