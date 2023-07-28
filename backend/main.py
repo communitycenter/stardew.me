bfac2cbd06e13ec56fc9a98dbe05aa09d3ab0e86
Jack LaFond <jack.lafond@icloud.com>
2023-07-27 22:41:16 -0400

    ERM

--- backend/main.py
+++ backend/main.py
@@ -43,10 +43,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
     expose_headers=["set-cookie"],  # Expose the "Set-Cookie" header to the browser
-    allow_headers=["*"],
-    allow_methods=["*"],
 )
 
 assets = loadAvatarAssets()
